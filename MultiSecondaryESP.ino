6c1f83d633f8224e889bda4d669a809da3999684
Matthew Adamson <2022madamson@gmail.com>
2025-11-06 13:16:04 -0500

    Add MultiSecondaryESP sketch for BLE scanning of multiple tags

---
+++ MultiSecondaryESP.ino
@@ -0,0 +1,143 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <map>
+#include <string>
+#include <BLEAdvertisedDevice.h>
+#include "RollingAverage.h"
+
+#define MAX_DEVICES 5
+
+BLEScan* pBLEScan;
+int scanTime = 1;
+
+// MMU MAC Addresses 
+BLEAddress targetDevices[] = {
+  BLEAddress("54:dc:e9:1d:34:ff")
+};
+
+int numTargetDevices = sizeof(targetDevices) / sizeof(targetDevices[0]);
+
+std::map<std::string, RollingAverage<int>> deviceRssiMap;
+
+// Gateway MAC Address
+uint8_t broadcastAddress[] = {0xD0, 0xCF, 0x13, 0x19, 0x93, 0x38};
+
+// Structure to hold data for a single device.
+typedef struct struct_device_data {
+  char mac_addr[18];
+  float average_rssi;
+} struct_device_data;
+
+// Structure to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  int device_count;
+  struct_device_data devices[MAX_DEVICES];
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const wifi_tx_info_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("Send Status: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
+}
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    std::string mac_str = advertisedDevice.getAddress().toString();
+
+    // Check if the found device is one we are tracking 
+    // .find() returns .end() if it does not find it 
+    if(deviceRssiMap.find(mac_str) != deviceRssiMap.end()) { 
+      int deviceRSSI = advertizedDevice.getRSSI();
+      deviceRssiMap[mac_str].add_value(deviceRSSI);
+    }
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  // wifi Setup
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  // Populate map with target devices.
+  Serial.println("Tracking devices: ");
+  for (int i = 0; i < numTargetDevices; i++) {
+    std::string mac_str = targetDevices[i].toString();
+    deviceRssiMap[mac_str] = RollingAverage<int>(10);
+    Serial.printf(" - %s\n", mac_str.c_str());
+  }
+
+  // BLE Setup
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan();  // create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);  // active scan -> uses more power but faster results
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal to setInterval value
+}
+
+
+void loop() {
+  pBLEScan->start(scanTime, false);
+
+  if (deviceRssiMap.empty()) {
+    Serial.println("No devices are being tracked.");
+    return;
+  }
+  
+  // Building the packet.
+  myData.device_count = 0;
+  const int min_readings_to_send = 5;
+
+  for(const auto& pair : deviceRssiMap) {
+    if (pair.second.get_count >= min_readings_to_send) {
+      if (myData.device_count >= MAX_DEVICES) {
+        Serial.println("Packet full, sending partial list.")
+        break;
+      }
+      // Copy MAC Address
+      strncpy(myData.devices[myData.device_count].mac_addr,
+              pair.first.c_str(),
+              sizeof(myData.devices[myData.device_count].mac_addr));
+      
+      myData.devices[myData.device_count].average_rssi = pair.second.get_average();
+      myData.device_count++;
+    }
+  }
+    
+    // Send packet over ESP-NOW.
+    if (myData.device_count > 0) {
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+      
+      if (result == ESP_OK) {
+        Serial.println("Sent continuous RSSI average with success");
+      } else {
+        Serial.println("Error sending the data");
+      }
+    }
+    else {
+      // This will only print if no packets have *ever* been received
+      Serial.println("No target device found yet...");
+    }  
+}
